53f45960a36c5ae9ab0aad021bd5b43c7492ae7c
adeline <adelineswires@gmail.com>
2021-11-04 16:33:38 -0700

    Auth pt. 2 Phase 0 initialized

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,9 +6,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 // // GET /api/set-token-cookie
 // const asyncHandler = require('express-async-handler');
