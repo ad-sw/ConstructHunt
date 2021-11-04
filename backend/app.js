27e132c0d5c832f489e41b00f83a1c98fb61df1d
adeline <adelineswires@gmail.com>
2021-11-04 13:16:51 -0700

    Auth pt. 1 Phase 2

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const routes = require('./routes');
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-
+const { ValidationError } = require('sequelize');
 
 // Security Middleware
 if (!isProduction) {
@@ -36,5 +36,36 @@ if (!isProduction) {
   );
 
 app.use(routes);  // Connect all the routes
+
+// ---defining error handlers after route connections---
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  // Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 module.exports = app;
