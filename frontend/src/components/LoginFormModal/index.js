0ffb473340359c9f0c30e775590a9fe6ef2e241a
adeline <adelineswires@gmail.com>
2021-11-05 15:39:25 -0700

    demo login

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
 import { Modal } from '../../context/Modal';
+import {login} from '../../store/session';
 import LoginForm from './LoginForm';
 
 function LoginFormModal() {
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
+  const demoLogin = async () => {
+    const demoUser = {credential: "demo@user.io", password: "password"};
+    await dispatch(login(demoUser))
+  }
 
   return (
+    <div className="RegUserDemoBtns">
     <>
-      <button onClick={() => setShowModal(true)}>Log In</button>
+      <button onClick={() => setShowModal(true)} className="loginBtn">Login</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
         </Modal>
       )}
     </>
+    <>
+    <button onClick={demoLogin} className="demoBtn">Demo Login</button>
+    </>
+    </div>
   );
 }
 
