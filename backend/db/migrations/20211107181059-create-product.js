f75538709cadb22f17f730907428172cabf14c1e
adeline <adelineswires@gmail.com>
2021-11-08 08:28:39 -0800

    updated DB defaultValue type for two data types in one file

--- backend/db/migrations/20211107181059-create-product.js
+++ backend/db/migrations/20211107181059-create-product.js
@@ -36,10 +36,12 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
+        defaultValue: Sequelize.fn("now"),
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        defaultValue: Sequelize.fn("now"),
         type: Sequelize.DATE
       }
     });
