40853338f17ba2cb669d97dac58a82d8702d6412
adeline <adelineswires@gmail.com>
2021-11-11 16:29:56 -0800

    slight change for Heroku

--- frontend/src/components/AboutMeFooter/AboutMe.js
+++ frontend/src/components/AboutMeFooter/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () =>{
             <a id='linkedin' href="https://linkedin.com/in/ad-sw" alt="LinkedIn">
                 <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png" alt="LinkedIn" width="20" height="20"></img>
               </a>
-              <a id='github' href="https://github.com/ad-sw/Construct-Hunt" alt="GitHub">
+              <a id='github' href="https://github.com/ad-sw" alt="GitHub">
                 <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F25%2F25231.png&f=1&nofb=1" alt="GitHub" width="20" height="20"></img>
               </a>
             </div>
