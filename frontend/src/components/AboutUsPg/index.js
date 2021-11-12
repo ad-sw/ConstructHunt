3a240a03bc63d9cdea2a9e71e6db705e0b153dba
adeline <adelineswires@gmail.com>
2021-11-12 10:11:57 -0800

    testing style about page

--- frontend/src/components/AboutUsPg/index.js
+++ frontend/src/components/AboutUsPg/index.js
@@ -1,18 +1,16 @@
 import './AboutUs.css';
 
 function AboutUs() {
-//   const dispatch = useDispatch();
-//   const sessionUser = useSelector(state => state.session.user);
-
 
   return (
-    <>
       <div className="descriptionAbout">
-        <center>
-            Construct Hunt surfaces the best new products, every day. It's a place for product-loving enthusiasts to share and geek out about the latest buildings, parks, city plans, neighborhoods, and hub centers in development.
-        </center>
+            <div id="textDesc">Construct Hunt surfaces the best new products, every day. It's a place for product-loving enthusiasts to share and geek out about the latest buildings, parks, city plans, neighborhoods, and hub centers in development.</div>
+            <div className="cropPicsAbout"><center>
+              <img id="banner" alt="test" src="https://media-exp1.licdn.com/dms/image/C5616AQGy6Fi8VKR87A/profile-displaybackgroundimage-shrink_200_800/0/1609543803357?e=1641427200&v=beta&t=P8mv8JT8vy3TCQuWpeRk_E8dcw4wx6_XzhtJ6KnJsRE"></img>
+              <img id="banner2" alt="test" src="https://media-exp1.licdn.com/dms/image/C5616AQGy6Fi8VKR87A/profile-displaybackgroundimage-shrink_200_800/0/1609543803357?e=1641427200&v=beta&t=P8mv8JT8vy3TCQuWpeRk_E8dcw4wx6_XzhtJ6KnJsRE"></img>
+              <img id="banner3" alt="test" src="https://media-exp1.licdn.com/dms/image/C5616AQGy6Fi8VKR87A/profile-displaybackgroundimage-shrink_200_800/0/1609543803357?e=1641427200&v=beta&t=P8mv8JT8vy3TCQuWpeRk_E8dcw4wx6_XzhtJ6KnJsRE"></img>
+            </center></div>
       </div>
-    </>
   );
 }
 
