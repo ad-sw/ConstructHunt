1ce8a7a4242f33a9f638aff2edda982f8b3b3e99
adeline <adelineswires@gmail.com>
2021-11-13 14:43:11 -0800

    styling changes to products page and product profiles

--- frontend/src/components/ProductProfilePage/index.js
+++ frontend/src/components/ProductProfilePage/index.js
@@ -7,6 +7,7 @@ import ProductModalUpdate from '../../components/ProductModal_Update';
 import ProductModalDelete from '../../components/ProductModal_Delete';
 import ReviewModal from '../../components/ReviewModal';
 import ReviewModalCreate from '../../components/ReviewModal_Create';
+import "./ProductProfile.css";
 
 function ProductProfilePgModal() {
     const sessionUser = useSelector(state => state.session.user);
@@ -20,11 +21,13 @@ function ProductProfilePgModal() {
 
     return (
         isLoaded && (
-        // <div>
-                <div key={product?.id}  className="products">
-                    <h2 id="productTitle">{product?.title}</h2>
-                    {<img alt="display" src={product?.imageUrl} height="25%" width="50%"></img>}
+        <>
+                <div key={product?.id}  className="productsProfile">
+                    <h2 id="prodProfileTitle">{product?.title}</h2>
+                    {<img className="profileImg" src={product?.imageUrl} height="25%" width="50%" alt="display"></img>}
                     <p>{product?.description}</p>
+                </div>
+                <div>
                     {sessionUser?.id === product?.userId &&
                     <><ProductModalUpdate product={product}/>
                     <ProductModalDelete product={product}/>
@@ -33,7 +36,7 @@ function ProductProfilePgModal() {
                     {<ReviewModal product={product}/>}
                     {sessionUser && <ReviewModalCreate productId={product?.id}/>}
                 </div>
-        // </div>
+        </>
         )
    );
 }
